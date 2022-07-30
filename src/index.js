10864defe4ed982b2098725ea3f14aa7d0261a83
Tamyres <tamyresfmelo@gmail.com>
2022-07-29 21:42:35 -0300

    Mensagem de validação na tela

--- src/index.js
+++ src/index.js
@@ -3,20 +3,25 @@ import validator from './validator.js';
 const nome = document.getElementById("nomeSobrenome")
 let cartao = document.getElementById("numeroCartao")
 let btnConfirmar = document.getElementById("pegarDadosCartao") 
+const mensagemValidacao = document.getElementById("validacao")
 
 
 btnConfirmar.addEventListener("click", function() {
      let cartaoValido = validator.isValid(cartao.value)
       if (cartaoValido == true){
-            alert (nome.value + ", Cartão Válido!")     
+            mensagemValidacao.innerHTML = nome.value + ", Cartão Válido!"   
        }else {
-            alert (nome.value + ", Cartão inválido!")
+          mensagemValidacao.innerHTML = nome.value + ", Cartão inválido!"
        }
+       
+      let mascaraCartao = validator.maskify(cartao.value)
+
+      cartao.value = mascaraCartao
 })
 
 
 
-console.log(validator.maskify(""))
+
 
 
 
